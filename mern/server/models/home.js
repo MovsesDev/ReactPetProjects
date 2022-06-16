c3ba80ec7cd3f2cacfe201a165cf85f2d715c985
movsesaghabekyan <movsesaghabekyan@icloud.com>
2022-06-16 16:54:45 +0400

    new

--- mern/server/models/home.js
+++
@@ -1,7 +0,0 @@
-const { Schema, model, Types } = require("mongoose");
-
-const schema = new Schema({
-  img: [String],
-});
-
-module.exports = model("Home", schema);
--- mern/server/models/homeList.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model, Types } = require("mongoose");
-
-const schema = new Schema({
-  img: [String],
-  owner: { type: String },
-});
-
-module.exports = model("HomeList", schema);
